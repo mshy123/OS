8781b00a2bd696a70cba04bd7d4fe133bb7c65be
6s <6s@vc031.kaist.ac.kr>
2015-03-16 14:41:59 +0300

    initial commit

---
+++ src/threads/build/tests/threads/hello-world.d
@@ -0,0 +1,4 @@
+tests/threads/hello-world.o: ../../tests/threads/hello-world.c \
+  ../../lib/stdio.h ../../lib/debug.h ../../lib/stdarg.h \
+  ../../lib/stdbool.h ../../lib/stddef.h ../../lib/stdint.h \
+  ../../lib/kernel/stdio.h ../../tests/threads/tests.h
